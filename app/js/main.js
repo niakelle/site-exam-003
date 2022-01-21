b703681f3eb64a7783e0a9bdb161950a107e628e
Kayn <morak0398@gmail.com>
2022-01-21 13:45:12 +0200

    on site

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,4 @@
-// import "slick-carousel";
-
+// функция, которая управляет слайдером
 let sliderWork = function () {
 	const slider = 	$(".slider");
 	let allSlides = $(".slider__slide");
@@ -47,6 +46,8 @@ let sliderWork = function () {
 	
 }
 
+// функция, которая управляет постами в теге .posts и переключением 
+// страниц там же
 let launchPosts = function () {
 	let currentActivePost = document.querySelector('.post.active');
 	const postsContainer = document.querySelector('.posts');
@@ -110,5 +111,65 @@ let launchPosts = function () {
 	clonePost();
 }
 
+// функция, которая собирает и отправляет сообщение в чат
+let sendMessage = function (messageContent) {
+
+	function createMessage (messageContent) {
+		let newMessage = document.createElement('div');
+		newMessage.classList.add('message', 'your__message');
+		// newMessage.innerHTML += `<h1>${messageContent}</h1>`;
+		newMessage.innerHTML += `
+		<h3 class="message__nickname">You</h3>
+		<p class="message__text">${messageContent}</p>
+		<span class="message__time">14:01</span>`;
+		return newMessage;
+	}
+	let newMessage = createMessage(messageContent);
+	
+	let chatContainer = document.querySelector('.chat__messages');
+	chatContainer.appendChild(newMessage);
+}
+
+// нахожу кнопку "отправить сообщение"
+const buttonSendMessage = document.querySelector('.chat__send-message');
+// привязываю к кнопке обработку клика - отправку сообщения
+buttonSendMessage.onclick = function (e) {
+	e.preventDefault();
+
+	// объявляю переменные
+	const inputElement = document.getElementById('send-message');
+	let inputValue = inputElement.value;
+
+	// запрещаю отправлять пустое сообщение в чат
+
+	if (inputValue.length <= 3 || !inputValue) {
+		alert('Ваше сообщение слишком короткое. Введите более трёх символов.');
+		clearInput();
+		return;
+	}
+	
+	// функция для перемещения скрытого скролла вниз контента в теге 
+	// .chat__messages
+	function updateScroll() {
+    let chatContainer = document.querySelector('.chat__messages');
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+	}
+
+	// формирую разметку сообщения и добавляю его в html с текстом из инпута
+	sendMessage(inputValue);
+
+	function clearInput () {
+		inputElement.value = '';
+	}
+
+	// очищаю поле ввода
+	clearInput();
+	// перемещаю скрытый скролл к последнему сообщению
+	updateScroll();
+}
+
+// запускаем слайдер
 sliderWork();
+
+// запускаем posts
 launchPosts();
